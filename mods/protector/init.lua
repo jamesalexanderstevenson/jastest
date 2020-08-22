8330c29aa6914e39da4f44b25220f27673197d68
James Stevenson <jastevenson303@gmail.com>
2020-08-22 10:46:06 -0400

    p

--- mods/protector/init.lua
+++ mods/protector/init.lua
@@ -192,91 +192,61 @@ end
 -- 3 for checking protector overlaps
 
 protector.can_dig = function(r, pos, digger, onlyowner, infolevel)
-
 	if not digger or not pos then
 		return false
 	end
-
 	-- protector_bypass privileged users can override protection
 	if infolevel == 1
 	and minetest.check_player_privs(digger, {protection_bypass = true}) then
 		return true
 	end
-
 	-- infolevel 3 is only used to bypass priv check, change to 1 now
 	if infolevel == 3 then infolevel = 1 end
-
 	-- is spawn area protected ?
 	if inside_spawn(pos, protector_spawn) then
-
 		minetest.chat_send_player(digger,
 			S("Spawn @1 has been protected up to a @2 block radius.",
 				minetest.pos_to_string(statspawn), protector_spawn))
-
 		return false
 	end
-
 	-- find the protector nodes
 	local pos = minetest.find_nodes_in_area(
 		{x = pos.x - r, y = pos.y - r, z = pos.z - r},
 		{x = pos.x + r, y = pos.y + r, z = pos.z + r},
 		{"protector:protect", "protector:protect2"})
-
 	local meta, owner, members
-
 	for n = 1, #pos do
-
 		meta = minetest.get_meta(pos[n])
 		owner = meta:get_string("owner") or ""
 		members = meta:get_string("members") or ""
-
 		-- node change and digger isn't owner
 		if infolevel == 1 and owner ~= digger then
-
 			-- and you aren't on the member list
 			if onlyowner or not is_member(meta, digger) then
-
 				hud.message(digger,
-				--minetest.chat_send_player(digger,
 					S("This area is owned by @1", owner) .. "!")
-
 				return false
 			end
 		end
-
 		-- when using protector as tool, show protector information
 		if infolevel == 2 then
-
 			hud.message(digger,
-			--minetest.chat_send_player(digger,
 				S("This area is owned by @1", owner) .. ".")
-
 			minetest.chat_send_player(digger,
 				S("Protection located at: @1", minetest.pos_to_string(pos[n])))
-
 			if members ~= "" then
-
 				minetest.chat_send_player(digger, S("Members: @1.", members))
 			end
-
 			return false
 		end
-
 	end
-
 	-- show when you can build on unprotected area
 	if infolevel == 2 then
-
 		if #pos < 1 then
-
 			hud.message(digger, S("This area is not protected."))
-			--minetest.chat_send_player(digger, S("This area is not protected."))
 		end
-
 		hud.message(digger, S("You can build here."))
-		--minetest.chat_send_player(digger, S("You can build here."))
 	end
-
 	return true
 end
 
