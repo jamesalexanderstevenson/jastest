eeaabea714f684d4a02aa172392bf28718b78bf3
James Stevenson <jastevenson303@gmail.com>
2020-09-07 06:56:44 -0400

    idk2

--- mods/mobs/api.lua
+++ mods/mobs/api.lua
@@ -1376,12 +1376,11 @@ end
 -- find and replace what mob is looking for (grass, wheat etc.)
 function mob_class:replace(pos)
 
-	if not mobs_griefing
-	or not self.replace_rate
-	or not self.replace_what
-	or self.child == true
-	or self.object:get_velocity().y ~= 0
-	or random(1, self.replace_rate) > 1 then
+	if not mobs_griefing or not self.replace_rate or
+			not self.replace_what or self.child == true or
+			not self.object:get_velocity() or
+			self.object:get_velocity().y ~= 0 or
+			random(1, self.replace_rate) > 1 then
 		return
 	end
 
