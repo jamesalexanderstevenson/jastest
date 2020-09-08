1544af9c202768bc7fc745b40870dee9a2729a4e
James Stevenson <jastevenson303@gmail.com>
2020-09-08 08:27:29 -0400

    try more

--- mods/water/init.lua
+++ mods/water/init.lua
@@ -1,8 +1,8 @@
 minetest.register_abm({
 	nodenames = {"group:melty"},
 	neighbors = {"group:igniter", "group:lava"},
-	interval = 10,
-	chance = 10,
+	interval = 30,
+	chance = 15,
 	catch_up = false,
 	action = function(pos, node)
 		minetest.set_node(pos, {name = "default:water_source"})
