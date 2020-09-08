0968db484a791853fd91a989c852ea08e63432b4
James Stevenson <jastevenson303@gmail.com>
2020-09-08 08:24:58 -0400

    try reduce load

--- mods/water/init.lua
+++ mods/water/init.lua
@@ -1,8 +1,8 @@
 minetest.register_abm({
 	nodenames = {"group:melty"},
 	neighbors = {"group:igniter", "group:lava"},
-	interval = 5,
-	chance = 5,
+	interval = 10,
+	chance = 10,
 	catch_up = false,
 	action = function(pos, node)
 		minetest.set_node(pos, {name = "default:water_source"})
