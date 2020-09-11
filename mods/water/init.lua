d95377bebbc31e097fa734bccfc7f9789adc6098
James Stevenson <jastevenson303@gmail.com>
2020-09-11 15:10:03 -0400

    faster melty for lava

--- mods/water/init.lua
+++ mods/water/init.lua
@@ -1,8 +1,8 @@
 minetest.register_abm({
 	nodenames = {"group:melty"},
 	neighbors = {"group:igniter", "group:lava"},
-	interval = 60,
-	chance = 50,
+	interval = 10,
+	chance = 100,
 	catch_up = true,
 	action = function(pos, node)
 		minetest.set_node(pos, {name = "default:water_source"})
