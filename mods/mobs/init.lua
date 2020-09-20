0b56300de4d725e72e58d82d31a8f2e7009b9ab9
James Stevenson <jastevenson303@gmail.com>
2020-09-20 11:14:49 -0400

    gah

--- mods/mobs/init.lua
+++ mods/mobs/init.lua
@@ -13,154 +13,147 @@ local threshold2 = 10
 local threshold2_dist = 10
 local mob_types = {}
 mobs.loho = function(player, pos)
-	local o = minetest.get_objects_inside_radius(pos, 16)
 	local lo = {x = pos.x + rand(-16, 16), y = pos.y + rand(-16, 16), z = pos.z + rand(-16, 16)}
 	local lon = minetest.get_node_or_nil(lo)
-	if lon then
-		if lon.name == "air" then
-			local lpo = {x = lo.x, y = lo.y - 1, z = lo.z}
-			local n = minetest.get_node_or_nil(lpo)
-			if not n then
+	if lon and lon.name == "air" then
+		local lpo = {x = lo.x, y = lo.y - 1, z = lo.z}
+		local n = minetest.get_node_or_nil(lpo)
+		if not n then
+			mobs.loho(nil, lo)
+			return
+		end
+		local nodef = minetest.registered_nodes[n.name]
+		local normal
+		if nodef then
+			normal = nodef.drawtype and (nodef.drawtype == "normal" or
+					nodef.drawtype == "plantlike")
+			if not normal then
 				mobs.loho(nil, lo)
 				return
 			end
-			local nodef = minetest.registered_nodes[n.name]
-			local normal
-			if nodef then
-				normal = nodef.drawtype and nodef.drawtype == "normal"
-				if not normal then
-					mobs.loho(nil, lo)
-					return
+		end
+		if n and n.name ~= "air" then
+			local ma = {}
+			for k, _ in pairs(mobs.spawning_mobs) do
+				ma[#ma + 1] = k
+			end
+			local maap = ma[rand(#ma)]
+			if not maap then
+				return
+			end
+			local animal = mob_types[maap] == "animal"
+			local hostile = mob_types[maap] == "monster"
+			local npc = mob_types[maap] == "npc"
+
+			local flower = n.name:find("flower")
+			local dirt = n.name:find("dirt")
+			local grass = n.name:find("grass")
+			local sand = n.name:find("sand")
+			local snow = n.name:find("snow")
+			local stone = n.name:find("stone")
+			local obsidian = n.name:find("obsidian")
+			local lava = n.name:find("lava")
+
+			local l1, l2, out_of_sight
+			if player then
+				l1 = player:get_look_dir()
+				l2 = vector.direction(pos, lpo)
+				out_of_sight = vector.distance(l1, l2) > 0.89
+			else
+				local s = minetest.get_objects_inside_radius(pos, 16)
+				local look = false
+				for k, v in pairs(s) do
+					if v:is_player() then
+						l1 = v:get_look_dir()
+						l2 = vector.direction(pos, lpo)
+						out_of_sight = vector.distance(l1, l2) > 0.89
+						if not out_of_sight then
+							look = true
+						end
+					end
+				end
+				if look then
+					out_of_sight = false
+				else
+					out_of_sight = true
 				end
 			end
-			if n and n.name ~= "air" then
-				--[[
-				local day = minetest.get_timeofday()
-				if day >= 0.45 and day <= 0.9 then
-					day = true
-				else
-					day = false
-				end
-				--]]
-				local ma = {}
-				for k, _ in pairs(mobs.spawning_mobs) do
-					ma[#ma + 1] = k
-				end
-				local maap = ma[rand(#ma)]
-				if not maap then
-					return
-				end
-				local animal = mob_types[maap] == "animal"
-				local hostile = mob_types[maap] == "monster"
-				local npc = mob_types[maap] == "npc"
-
-				local flower = n.name:find("flower")
-				local dirt = n.name:find("dirt")
-				local grass = n.name:find("grass")
-				local sand = n.name:find("sand")
-				local snow = n.name:find("snow")
-				local stone = n.name:find("stone")
-				local obsidian = n.name:find("obsidian")
-				local lava = n.name:find("lava")
-
-				local l1, l2, out_of_sight
-				if player then
-					l1 = player:get_look_dir()
-					l2 = vector.direction(pos, lpo)
-					out_of_sight = vector.distance(l1, l2) > 0.89
-				else
-					local s = minetest.get_objects_inside_radius(pos, 16)
-					local look = false
-					for k, v in pairs(s) do
-						if v:is_player() then
-							l1 = v:get_look_dir()
-							l2 = vector.direction(pos, lpo)
-							out_of_sight = vector.distance(l1, l2) > 0.89
-							if not out_of_sight then
-								look = true
-							end
+			if out_of_sight then
+				local o = minetest.get_objects_inside_radius(pos, 16)
+				if (animal or npc) and (dirt or snow or grass) and
+						lpo.y >= 0 and not night.night then
+					if maap:find("sheep") then
+						maap = "mobs:sheep_white"
+					end
+					if snow and rand() >= 0.5 then
+						maap = "mobs:penguin"
+					end
+					if flower then
+						maap = "mobs:bee"
+					end
+					local c = 0
+					for k, v in pairs(o) do
+						local loa = v:get_luaentity()
+						if loa and loa.health then
+							c = c + 1
 						end
 					end
-					if look then
-						out_of_sight = false
+					local ob = minetest.get_objects_inside_radius(lo, 16)
+					for k, v in pairs(ob) do
+						local loa = v:get_luaentity()
+						if loa and loa.health then
+							c = c + 1
+						end
 					end
-				end
-				if out_of_sight then
-					if (animal or npc) and (dirt or snow or grass) and
-							lpo.y >= 0 and not night.night then
-						if maap:find("sheep") then
-							maap = "mobs:sheep_white"
+					if c < threshold1 and 
+							vector.distance(pos, lo) > threshold1_dist then
+						minetest.add_entity(lo, maap)
+						--[[
+						print("top", "night:", night.night, lpo.y, os.date(),
+								maap, "prot:", minetest.is_protected(lpo, ""))
+						--]]
+					end
+				elseif (hostile or npc) and
+						(stone or obsidian or grass or dirt or sand) and
+						(lpo.y < -16 or night.night) then
+					if maap == "mobs:lava_flan" and not lava then
+						maap = "mobs:oerkki"
+					elseif lava then
+						maap = "mobs:lava_flan"
+					elseif sand and rand() >= 0.5 then
+						maap = "mobs:sand_monster"
+					elseif dirt and rand() >= 0.5 then
+						maap = "mobs:dirt_monster"
+					end
+					if minetest.is_protected(lpo, "") or rand() >= 0.9 then
+						maap = "mobs:rat"
+						if rand() < 0.1 then
+							maap = "mobs:npc"
+						elseif rand() >= 0.95 then
+							maap = "mobs:trader"
 						end
-						if snow and rand() >= 0.5 then
-							maap = "mobs:penguin"
+					end
+					local c = 0
+					for k, v in pairs(o) do
+						local loa = v:get_luaentity()
+						if loa and loa.health then
+							c = c + 1
 						end
-						if flower then
-							maap = "mobs:bee"
+					end
+					local ob = minetest.get_objects_inside_radius(lo, 16)
+					for k, v in pairs(ob) do
+						local loa = v:get_luaentity()
+						if loa and loa.health then
+							c = c + 1
 						end
-						local c = 0
-						for k, v in pairs(o) do
-							local loa = v:get_luaentity()
-							if loa and loa.health then
-								c = c + 1
-							end
-						end
-						local ob = minetest.get_objects_inside_radius(lo, 16)
-						for k, v in pairs(ob) do
-							local loa = v:get_luaentity()
-							if loa and loa.health then
-								c = c + 1
-							end
-						end
-						if c < threshold1 and 
-								vector.distance(pos, lo) > threshold1_dist then
-							minetest.add_entity(lo, maap)
-							--[[
-							print("top", "night:", night.night, lpo.y, os.date(),
-									maap, "prot:", minetest.is_protected(lpo, ""))
-							--]]
-						end
-					elseif (hostile or npc) and
-							(stone or obsidian or grass or dirt or sand) and
-							(lpo.y < -16 or night.night) then
-						if maap == "mobs:lava_flan" and not lava then
-							maap = "mobs:oerkki"
-						elseif lava then
-							maap = "mobs:lava_flan"
-						elseif sand and rand() >= 0.5 then
-							maap = "mobs:sand_monster"
-						elseif dirt and rand() >= 0.5 then
-							maap = "mobs:dirt_monster"
-						end
-						if minetest.is_protected(lpo, "") or rand() >= 0.9 then
-							maap = "mobs:rat"
-							if rand() < 0.1 then
-								maap = "mobs:npc"
-							elseif rand() >= 0.95 then
-								maap = "mobs:trader"
-							end
-						end
-						local c = 0
-						for k, v in pairs(o) do
-							local loa = v:get_luaentity()
-							if loa and loa.health then
-								c = c + 1
-							end
-						end
-						local ob = minetest.get_objects_inside_radius(lo, 16)
-						for k, v in pairs(ob) do
-							local loa = v:get_luaentity()
-							if loa and loa.health then
-								c = c + 1
-							end
-						end
-						if c < threshold2 and
-								vector.distance(pos, lo) > threshold2_dist then
-							minetest.add_entity(lo, maap)
-							--[[
-							print("bottom", "night:", night.night, lpo.y, os.date(),
-									maap, "prot:", minetest.is_protected(lpo, ""))
-							--]]
-						end
+					end
+					if c < threshold2 and
+							vector.distance(pos, lo) > threshold2_dist then
+						minetest.add_entity(lo, maap)
+						--[[
+						print("bottom", "night:", night.night, lpo.y, os.date(),
+								maap, "prot:", minetest.is_protected(lpo, ""))
+						--]]
 					end
 				end
 			end
@@ -218,7 +211,6 @@ function mobs.show_gui(name)
 				ent = mobs.players[name].ent
 			end
 			if not ent then
-				print("?!??")
 				return
 			end
 			local hp = ent.health
@@ -326,7 +318,6 @@ minetest.register_on_player_receive_fields(function(player, formname, fields)
 					p:set_pos(player:get_pos())
 				end
 			else
-				print("aww")
 			end
 		end
 	elseif fields.leash then
@@ -398,4 +389,3 @@ dofile(path .. "/zombies.lua")
 for k, v in pairs(mobs.spawning_mobs) do
 	mob_types[k] = minetest.registered_entities[k].type
 end
---print(dump(mob_types))
